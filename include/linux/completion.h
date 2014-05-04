8c40d847ada860ac3458f59b45d076de33a038bb
Schischu <schischu65@gmail.com>
2014-05-04 21:44:18 +0200

    wiimote: Downgrade driver from kernel 3.4

--- include/linux/completion.h
+++ include/linux/completion.h
@@ -19,7 +19,7 @@
  *
  * See also:  complete(), wait_for_completion() (and friends _timeout,
  * _interruptible, _interruptible_timeout, and _killable), init_completion(),
- * and macros DECLARE_COMPLETION(), DECLARE_COMPLETION_ONSTACK(), and
+ * reinit_completion(), and macros DECLARE_COMPLETION(), DECLARE_COMPLETION_ONSTACK(), and
  * INIT_COMPLETION().
  */
 struct completion {
@@ -74,6 +74,18 @@ static inline void init_completion(struct completion *x)
 {
 	x->done = 0;
 	init_waitqueue_head(&x->wait);
+}
+
+/**
+ * reinit_completion - reinitialize a completion structure
+ * @x:  pointer to completion structure that is to be reinitialized
+ *
+ * This inline function should be used to reinitialize a completion structure so it can
+ * be reused. This is especially important after complete_all() is used.
+ */
+static inline void reinit_completion(struct completion *x)
+{
+	x->done = 0;
 }
 
 extern void wait_for_completion(struct completion *);
